75f9e851d2898bc5ba88b727ec96e8281465c510
Caitano <josiasmartinscaitano@gmail.com>
2022-01-10 17:52:33 -0300

    sintaxe get

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -9,20 +9,20 @@ class Negociacao {
         this._quantidade = quantidade;
         this._valor = valor;
     } 
-
-    getVolume() {
+    // get: propriedade de acesso leitura
+    get volume() {
 
         return this._quantidade * this._valor;
     }
 
-    getData() {
+    get data() {
        return this._data;
     }
-    getQuantidade() {
+    get quantidade() {
        return this._quantidade;
     }
 
-    getValor() {
+    get valor() {
         return this._valor;
     }
 }
