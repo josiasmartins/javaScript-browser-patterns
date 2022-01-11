98dcc23ad54f4b6a3a05d9453c0bfb662dbb02ba
Caitano <josiasmartinscaitano@gmail.com>
2022-01-11 13:58:09 -0300

    finalizado a seção

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -34,4 +34,45 @@ class NegociacaoController {
 
             console.log(negociacao);
     }
+
 }
+
+function somaDoisNumber(num1, num2) {
+    return num1 + num2;
+}
+
+// console.log(somaDoisNumber(30, 20));
+
+
+let numeros = [10, 30];
+// console.log(somaDoisNumber(...numeros, 'ibag'));
+
+class Aluno {
+
+    constructor(matricula, nome) {
+        this.matricula = matricula;
+        this.nome = nome;
+    }
+}
+
+class Prova {
+
+    constructor(aluno, nota) {
+        this.aluno = aluno;
+        this.nota = nota;
+    }
+}
+
+let avaliacoes = [
+    new Prova(new Aluno(1, 'Luana'), 8),
+    new Prova(new Aluno(2, 'Cássio'), 6),
+    new Prova(new Aluno(3, 'Barney'), 9),
+    new Prova(new Aluno(4, 'Bira'), 5)
+];
+
+
+let aprovados = avaliacoes
+    .filter(prova => prova.nota >= 7 )
+    .map(prova => prova.aluno.nome);
+console.log(aprovados)
+
