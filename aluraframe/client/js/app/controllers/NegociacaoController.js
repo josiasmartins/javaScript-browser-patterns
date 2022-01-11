4e7347bc5d2fec4535dc1570ee9e9036660d1951
Caitano <josiasmartinscaitano@gmail.com>
2022-01-11 07:40:09 -0300

    pegando os elementos através do document.querySelector na classe NegociacaoController

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -2,8 +2,17 @@ class NegociacaoController {
 
     adiciona(event) {
         // cancela a submissão do formulário
+        // TODO: erro no preventDeafult
         event.preventDefault();
-        alert('Chamei a ação no controller');
-        console.log("ibag adiciona()")
+        // bind: mantem uma associação comm document
+        let $ = document.querySelector.bind(document);
+
+        let inputData = $('#data');
+        let inputQuantidade = $('#quantidade');
+        let inputValor = $('#valor');
+
+        console.log(inputData.value);
+        console.log(inputQuantidade.value);
+        console.log(inputValor.value);
     }
 }
