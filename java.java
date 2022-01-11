cebb219f2f512774dc5db5a6bc57dc7718dbad0f
Caitano <josiasmartinscaitano@gmail.com>
2022-01-11 06:57:22 -0300

    adicionado mais uma condicional

--- java.java
+++ java.java
@@ -2,10 +2,15 @@ class TestaCondicional {
     public static void main(String[] args) {
         System.out.println("Ola peopel");
 
-        int valor = 10;
+        int valor = 3;
 
         if (valor > 1 && valor < 5) {
+            if (valor == 3) {
+                System.out.println("ibag 3. Happy birds");
+            } else if (valor == 4) return;
             System.out.println("vc tem entre 1 e 5");
         } else System.out.println("voce é maior");
+
+
     }
 }
