b5aa122e14431d1b17bf7281eee90c82b6a88969
Caitano <josiasmartinscaitano@gmail.com>
2022-01-11 07:56:28 -0300

    criando a instancia de negociacao

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,18 +1,23 @@
 class NegociacaoController {
 
+    constructor() {
+        // bind: mantem uma associação comm document
+        let $ = document.querySelector.bind(document);
+
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+
     adiciona(event) {
         // cancela a submissão do formulário
         // TODO: erro no preventDeafult
         event.preventDefault();
-        // bind: mantem uma associação comm document
-        let $ = document.querySelector.bind(document);
 
-        let inputData = $('#data');
-        let inputQuantidade = $('#quantidade');
-        let inputValor = $('#valor');
-
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value);
+        console.log(typeof(this._inputData.value));
+        
+        console.log(this._inputData.value);
+        console.log(negociacao);
+         // adicionar a negociacao em uma lista
     }
 }
